c7b8cbfd980f92f35b2a2e611071abfac8879742
kwdaisuke <daisuke0582@gmail.com>
2022-10-10 12:05:12 +0900

    change

--- babushka/main.py
+++ babushka/main.py
@@ -4,6 +4,7 @@
 import json
 import tempfile
 import warnings
+import inquirer
 from argparse import Namespace
 from datetime import datetime
 from pathlib import Path
@@ -26,6 +27,27 @@ from babushka import data, models, predict, train, utils
 app = typer.Typer()
 
 @app.command()
+def ELT_data():
+    questions = [inquirer.List("model_type", message="Which model to be used?",
+                choices=['Table', 'Image', 'Language']),
+                inquirer.List("table_type", message="Which table type to be used?",
+                choices =['Regression', "Classification", "Time Series"])]
+    # Extract, Load, Transform
+    from google.cloud import aiplatform
+    aiplatform.TabularDataset.create(
+        display_name=display_name,
+        gcs_source=gcs_source
+
+    )
+    dataset.wait()
+
+    print(f'\tDataset: "{dataset.display_name}')
+    print(f'\tname: "{dataset.resource_name}"')
+
+    
+    
+
+@app.command()
 def download_auxiliary_data():
     print("test")
 
@@ -38,20 +60,31 @@ def compute_feature():
     pass
 
 @app.command()
-def trainer(display_name: str, bq_source: str):
+def trainer(dataset_id: str,):
     """
     Args:
     bq_source = "bq://project.dataset.table_name"
     """
-    from google.cloud import aiplatform
-    dataset = aiplatform.TabularDataset.create(
-        display_name=display_name,
-        bq_source = bq_source)
     
-    dataset.wait()
+    # Retrieve existing dataset
+    from google.cloud import aiplatform
+    dataset = aiplatform.TabularDataset.(dataset_id)
     train.train(dataset)
+
+
+def save_artifacts():
+    questions = [inquirer.List("model_type", message="Which model to be used?",
+                choices=['Table', 'Image', 'Language']),
+                inquirer.List("table_type", message="Which table type to be used?",
+                choices =['Regression', "Classification", "Time Series"])]
+
+    models.save_model(questions["model_type"], questions="table_type")
 
 @app.command()
 def load_artifacts():
+    questions = [inquirer.List("model_type", message="Which model to be used?",
+                choices=['Table', 'Image', 'Language']),
+                inquirer.List("table_type", message="Which table type to be used?",
+                choices =['Regression', "Classification", "Time Series"])]
+
     model = models.load_model()
-    pass
--- babushka/models.py
+++ babushka/models.py
@@ -1,7 +1,7 @@
 import math
 from argparse import Namespace
 from typing import List, Dict
-#import google.cloud.aiplatform as aip
+import google.cloud.aiplatform as aip
 import tensorflow as tf
 import tensorflow_hub as hub
 
@@ -10,57 +10,94 @@ project="project-daisuke318420"
 location="us-central1"
 display_name="AutoML"
 
-def load_model():
+def save_model(model_type: str = None, table_type: str=None):s
     """
     TODO: Build sklearn classification model
     """
     CONCRETE_INPUT = "numpy_inputs"
+    BUCKET_NAME = "[your-bucket-name]"  # @param {type:"string"}
+    BUCKET_URI = f"gs://{BUCKET_NAME}"
+    MODEL_DIR = BUCKET_URI + "/model"
 
     ```Image classification Model```
 
     # Download the pretrained model from Tensorflow Hub
+    if model_type = "table":
+        if table_type="regression":
 
-    tfhub_model = tf.keras.Sequential(
-        [hub.KerasLayer("https://tfhub.dev/google/imagenet/resnet_v2_101/classification/5")]
-    )
+        elif table_type="classification":
 
-    tfhub_model.build([None, 224, 224, 3])
-    print(tfhub_model.summary())
+        else:
+            return False
 
-    def _preprocess(bytes_input):
-        decoded = tf.io.decode_jpeg(bytes_input, channels = 3)
-        decoded = tf.image.conver_image_dtype(decoded, tf.loat32)
-        resized = tf.image.resize(decoded, size=(224, 224))
-        return resized
+    elif model_type = "image":
+        tfhub_model = tf.keras.Sequential(
+            [hub.KerasLayer("https://tfhub.dev/google/imagenet/resnet_v2_101/classification/5")]
+        )
+
+        tfhub_model.build([None, 224, 224, 3])
+        print(tfhub_model.summary())
+
+        def _preprocess(bytes_input):
+            decoded = tf.io.decode_jpeg(bytes_input, channels = 3)
+            decoded = tf.image.conver_image_dtype(decoded, tf.loat32)
+            resized = tf.image.resize(decoded, size=(224, 224))
+            return resized
         
+        @tf.function(input_signature=[tf.TensorSpec([None], tf.string)])
+        def preprocess_fn(bytes_inputs):
+            decoded_images = tf.map_fn(_preprocess, bytes_inputs, dtype=tf.loat32, back_prop = False)
+            return {CONCRETE_INPUT: decoded_images }
+            # User needs to make sure the key matches model's input
+
+        @tf.function(input_signature=[tf.TensorSpec([None], tf.string)])
+        def serving_fn(bytes_inputs):
+            images = preprocess_fn(bytes_inputs)
+            prob = m_call(**images)
+            return prob
+        
+        m_call = tf.function(tfhub_model.call).get_concrete_function(
+            [tf.TensorSpec(shape=[None, 224, 224, 3], dtype = tf.float32, name=CONCRETE_INPUT)]
+        )
+
+        tf.saved_model.save(tfhub_model, MODEL_DIR, signatures={"serving_default": serving_fn})
+
     
-    @tf.function(input_signature=[tf.TensorSpec([None], tf.string)])
-    def preprocess_fn(bytes_inputs):
-        decoded_images = tf.map_fn(_preprocess, bytes_inputs, dtype=tf.loat32, back_prop = False)
-        return {CONCRETE_INPUT: decoded_images }
-        # User needs to make sure the key matches model's input
+def load_model(model_type = None, table_type=None):
 
-    @tf.function(input_signature=[tf.TensorSpec([None], tf.string)])
-    def serving_fn(bytes_inputs):
-        images = preprocess_fn(bytes_inputs)
-        prob = m_call(**images)
-        return prob
-    
-    m_call = tf.function(tfhub_model.call).get_concrete_function(
-        [tf.TensorSpec(shape=[None, 224, 224, 3], dtype = tf.float32, name=CONCRETE_INPUT)]
-    )
+    #Loading process
+    loaded = tf.saved_model.load(MODEL_DIR)
+    #serving_input = list(loaded.signatures["serving_default"].structured_input_signatures[1].keys())
 
-tf.saved_model.save(tfhub_model, MODEL_DIR, signatures={"serving_default": serving_fn})
-
-    pass
-
-def load_automl():
+def load_automl(model_type = None, table_type=None):
     aiplatform.init(project=project, location=location)
-    model = aiplatform.AutoMLTabularTrainingJob(
-            display_name=display_name,
-            optimization_prediction_type="classification"
+    if model_type = "table":
+        if table_type="regression":
+            model = aiplatform.AutoMLTabularTrainingJob(
+                display_name="table regressionmodel",
+                optimization_prediction_type="regression"
+                )
+
+        elif table_type="classification":
+            model = aiplatform.AutoMLTabularTrainingJob(
+                display_name="table classification model",
+                optimization_prediction_type="classification"
+                )
+        elif table_type="time":
+            model = aiplatform.AutoMLForecastingTrainingJob(
+                dispaly_name="table time model",      
             )
-    return model
+        else:
+            return False
+
+        return model
+
+    if model_type="image":
+        model = aiplatform.AutoMLImageTrainingJob(
+                display_name=display_name,
+                prediction_type="classification"
+                )
+        return model
 
 def initialize_model(auto: False):
     if auto:
