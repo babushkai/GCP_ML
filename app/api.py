7c926069169134eab56ad89fa260d24419fc19de
kwdaisuke <daisuke0582@gmail.com>
2022-10-16 14:18:38 +0900

    change

--- app/api.py
+++ app/api.py
@@ -15,9 +15,24 @@ app = FastAPI(
 )
 
 def wrapper():
-    pass
+    @wraps(f)
+    def wrap(request: Request, *args, **kwargs) -> Dict:
+        results = f(request, *args, **kwargs)
+        response = {
+            "message": results["message"],
+            "method": request.method,
+            "status-code": results["status-code"],
+            "timestamp": datetime.now.isoformat(),
+            "url": request.url._url,
+        }
+        if "data" in results:
+            response["data"] = results["data"]
+        return response
+    return wrap
+
 
 @app.get("/")
+@wrapper
 def _index() -> Dict:
     """Health check."""
     response = {
