07ea7915164330e124b99e5614372a4e92139add
kwdaisuke <daisuke0582@gmail.com>
2022-10-18 06:38:25 +0900

    change

--- tests/babushka/test_models.py
+++ tests/babushka/test_models.py
@@ -1,7 +1,8 @@
 from babushka import models
+from google.cloud import aiplatform
 
 def test_is_model():
-    assert(models.load_automl() == True)
+    assert(models.load_automl() == aiplatform.Models)
 
 def test_initialize_model():
     assert(models.initialize_model() == True)
