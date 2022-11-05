03d359f96f702af4529455f1932513e91f7306a7
kwdaisuke <daisuke0582@gmail.com>
2022-11-05 14:18:19 +0900

    change

--- config/config.py
+++ config/config.py
@@ -4,6 +4,25 @@ from pathlib import Path
 
 from rich.logging import RichHandler
 
+# Directories
+BASE_DIR = Path(__file__).parent.parent.absolute()
+CONFIG_DIR = Path(BASE_DIR, "config")
+DATA_DIR = Path(BASE_DIR, "data")
+STORES_DIR = Path(BASE_DIR, "stores")
+LOGS_DIR = Path(BASE_DIR, "logs")
+
+# Stores
+MODEL_REGISTRY = Path(STORES_DIR, "model")
+BLOB_STORE = Path(STORES_DIR, "blob")
+
+# Create dirs
+DATA_DIR.mkdir(parents=True, exist_ok=True)
+MODEL_REGISTRY.mkdir(parents=True, exist_ok=True)
+LOGS_DIR.mkdir(parents=True, exist_ok=True)
+BLOB_STORE.mkdir(parents=True, exist_ok=True)
+
+#
+
 # Logger
 logging_config = {
     "version": 1,
